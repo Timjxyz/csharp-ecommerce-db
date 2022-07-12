442491a42c41c1f28b36c9aac154e0cb990ca209
Timothy Gaither <timjgaither@gmail.com>
2022-07-12 16:05:55 +0200

    Inserimento di tre prodotti nel database

--- Program.cs
+++ Program.cs
@@ -9,4 +9,44 @@
 //4. modificare l’ordine di un cliente
 //5. cancellare un ordine di un cliente
 //6. cancellare un prodotto su cui è attivo almeno un ordine
-Console.WriteLine("Hello, World!");
+using (EcommercContext db = new EcommercContext())
+{
+
+    //Creo tre prodotti
+
+    //Product piscina = new Product();
+    //piscina.Name = "Piscina Idromassaggio Gonfiabile";
+    //piscina.Description = "Per massimo 6 persone Il pannello di controllo digitale di facile accesso riscalda l'acqua fino a 40 ° C";
+    //piscina.Price = 414;
+    
+    //Product asciugamano = new Product();
+    //asciugamano.Name = "Asciugamano da Spiaggia";
+    //asciugamano.Description = "Il telo mare è dotato esclusivamente di un'etichetta UV che cambia colore";
+    //asciugamano.Price = 15;
+
+    //Product materassino = new Product();
+    //materassino.Name = "Materassino Gonfiabile";
+    //materassino.Description = "Può convertirsi in amaca, sedia, drifter o sella per esercizi. Godetevi quattro diversi tipi di divertimento in una sola volta.";
+    //materassino.Price = 14;
+
+    //db.Add(piscina);
+    //db.Add(asciugamano);
+    //db.Add(materassino);
+    //db.SaveChanges();
+
+    Console.WriteLine("\t****Lista prodotti mare****");
+    List<Product> products = db.Products.OrderBy(product => product.Price).ToList<Product>();
+
+    foreach(Product product in products)
+    {
+        Console.WriteLine("");
+        Console.WriteLine(product.Name);
+        Console.WriteLine(product.Description);
+        Console.WriteLine($"{product.Price} euro");
+        Console.WriteLine("");
+    }
+
+
+
+
+}
